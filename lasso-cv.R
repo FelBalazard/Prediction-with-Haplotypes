3727e5ffd11d6aa20440f753cf6da8e2ea8a01e4
FelBalazard <felbalazard@hotmail.fr>
2016-02-02 12:31:09 +0100

    Create lasso-cv.R

---
+++ lasso-cv.R
@@ -0,0 +1,41 @@
+ #!/usr/bin/Rscript
+
+args<-commandArgs(TRUE)
+
+fold<-as.numeric(args[1])
+letter<-args[2]
+window<-args[3]
+minsample<-args[4]
+
+#lasso.R by Félix Balazard UPMC/INSERM
+
+#This script performs logistic regression with L1 penalization on the evidence from
+#each block. It starts by regrouping the results of disc.bloc* in disc.bloc1 
+#(respectively valid).
+#It then performs lasso as implemented by glmnet and calculates AUC as implemented 
+#by AUC. The resulting R workspace is saved under resultat.Rdata.
+
+#Input : disc.bloc* valid.bloc*
+#Output : resultat.Rdata resultat (the AUC in the validation set)
+#Packages needed : glmnet and AUC
+
+bloc.info<-read.csv("bloc.info", sep=" ",header=F)
+#blocksize<-read.csv("blocksize",sep=" ",header=F)
+
+disc<-t(read.csv(paste(letter,".disc.bloc1",sep=""),sep=" ",header=F))
+disc.label<-as.matrix(read.csv("discovery.label",sep=" ",header=F))
+valid<-t(read.csv(paste(letter,".valid.bloc1",sep=""),sep=" ",header=F))
+library("glmnet")
+
+#save.image(paste("resultat_",letter,"_",fold,".Rdata",sep=""))
+
+for (numbloc in c(300,500,700)){
+model<-cv.glmnet(disc[,1:numbloc],disc.label,family="binomial",type.measure="auc")
+pred<-predict(model,valid[,1:numbloc])
+valid.sample<-read.csv("validation.phased.chr1.sample",sep=" ",header=T)
+valid.sample<-valid.sample[-1,]
+library("AUC")
+resultat<-auc(roc(pred,valid.sample[,7]))
+resultat
+system(paste("echo",fold,resultat,">>",paste(letter,numbloc,window,minsample,sep="."),sep=" "))}
+#save.image(paste("resultat_",letter,"_",fold,".Rdata",sep=""))
